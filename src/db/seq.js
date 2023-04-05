7ee2882a0bae2a21afb3e5d57941301138e7d35f
xhir <syh1508@163.com>
2023-04-06 01:26:36 +0800

    连接MySQL数据库

---
+++ src/db/seq.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize')
+
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PWD,
+  MYSQL_DB
+} = require('../config/config.default')
+const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
+  host: MYSQL_HOST,
+  dialect: 'mysql',
+  // timezone: '+08:00'
+})
+
+module.exports = seq
+
+// 测试数据库是否连接成功
+// seq.authenticate().then(() => {
+//   console.log("数据库连接成功")
+// }).catch((err) => {
+//   console.log("数据库连接失败", err)
+// })
