f3325639a5fa609df9adb2ce56b725f6c01893d9
xhir <syh1508@163.com>
2023-04-06 00:17:43 +0800

    添加service层操作数据库

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,16 +1,21 @@
+const { createUser } = require('../service/user.service')
 class UserController {
   /**
    * 注册接口
    */
-  async register(ctx, next) {
-    console.log('注册接口', ctx.request.body)
-    ctx.body = ctx.request.body
+  async register (ctx, next) {
+    // 1. 获取接口传入的数据
+    const { user_name, password } = ctx.request.body
+    // 2. 操作数据库
+    const res = await createUser(user_name, password)
+    // 3. 返回结果
+    ctx.body = res
   }
 
   /**
    * 登录接口
    */
-  async login(ctx, next) {
+  async login (ctx, next) {
     ctx.body = '用户登录接口'
   }
 }
