9c209afb39c89ca0085ed850514e1dc070a8ff9d
xhir <syh1508@163.com>
2023-04-07 00:09:47 +0800

    增加接口的错误处理逻辑

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-const { createUser } = require('../service/user.service')
+const { createUser, getUserInfo } = require('../service/user.service')
 class UserController {
   /**
    * 注册接口
@@ -6,6 +6,33 @@ class UserController {
   async register (ctx, next) {
     // 1. 获取接口传入的数据
     const { user_name, password } = ctx.request.body
+    /**
+     * 错误处理
+     * 1. 合法性
+     * 2. 合理性
+     */
+    // 1. 合法性
+    if (!user_name || !password) {
+      console.error('用户名或密码为空', ctx.request.body)
+      ctx.status = 400
+      ctx.body = {
+        code: 10001,
+        msg: '用户名或密码为空',
+        data: null
+      }
+      return
+    }
+    // 2. 合理性
+    if (await getUserInfo({ user_name })) {
+      // 资源冲突
+      ctx.status = 409
+      ctx.body = {
+        code: 10002,
+        msg: '用户已经存在',
+        data: null
+      }
+      return
+    }
     // 2. 操作数据库
     const res = await createUser(user_name, password)
     // 3. 返回结果
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,9 +1,37 @@
 const UserModel = require('../model/user.model')
 class UserService {
+  /**
+   * 在数据库中新建一个用户
+   * @param {string} user_name 用户名
+   * @param {string} password 密码
+   * @returns 创建的用户信息
+   */
   async createUser (user_name, password) {
     const res = await UserModel.create({ user_name, password })
     return res.dataValues
   }
+
+  /**
+   * 根据条件查询用户
+   * @param {object} param0 查询条件
+   * @returns 查询到的用户
+   */
+  async getUserInfo ({ id, user_name, password, is_admin }) {
+    const whereOpt = {}
+
+    id && Object.assign(whereOpt, { id })
+    user_name && Object.assign(whereOpt, { user_name })
+    password && Object.assign(whereOpt, { password })
+    is_admin && Object.assign(whereOpt, { is_admin })
+
+    const res = await UserModel.findOne({
+      attributes: ['id', 'user_name', 'password', 'is_admin'],
+      where: whereOpt
+    })
+
+    return res ? res.dataValues : null
+  }
+
 }
 
 module.exports = new UserService()
